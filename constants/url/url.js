2e11c49551e82774b5a67785e5d1a1b84fe3a6ab
Shashikumar03 <shashikumarkushwaha3@gmail.com>
2024-08-26 02:23:52 +0530

    deploy to expo

--- constants/url/url.js
+++ constants/url/url.js
@@ -1,7 +1,7 @@
 export function getBaseUrl() {
-  // return "https://library-api-production-22cd.up.railway.app"
+  return "https://library-api-production-22cd.up.railway.app"
   // "anty wifi"
-    return "http://192.168.0.189:8283";
+    // return "http://192.168.0.189:8283";
     // "vishal"
     // return "http://192.168.73.125:8283";
     // "shashi"
